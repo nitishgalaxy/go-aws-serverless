8669b524a71c6aa316972453cffee01f7c9f1222
Nitish Saha <nitisaha@cisco.com>
2021-03-17 20:00:41 +0530

    Save changes

---
+++ lambda-go-1/deploy.sh
@@ -0,0 +1,3 @@
+# Remember to build your handler executable for Linux!
+GOOS=linux GOARCH=amd64 go build -o main main.go
+zip main.zip main
---
+++ lambda-go-1/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+type Event struct {
+	Username string
+}
+
+func handler(e Event) (string, error) {
+	// return output, err
+	fmt.Println(e)
+	return fmt.Sprintf("<h1>Hello %s </h1>", e.Username), nil
+}
+
+func main() {
+	lambda.Start(handler)
+}
